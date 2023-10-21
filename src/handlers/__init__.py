2a4695e07226b3e3554ef5903677d96a8dddb2f2
glebishead <glebaskayumov@yandex.ru>
2023-10-21 16:26:32 +0200

    Added: static directory Changed: __init__.py, user.py Несколько новых функций и заглушка

--- src/handlers/__init__.py
+++ src/handlers/__init__.py
@@ -2,14 +2,20 @@
 
 """
 
-from src import dp
+from src import dp, bot
 from .admin import *
 from .user import *
 
 
 def register_user_handlers():
 	dp.register_message_handler(start, commands=['start'])
-	dp.register_message_handler(echo)
+	
+	# Обработчики событий от пользователя
+	dp.register_message_handler(send_contacts, commands=['contacts'])
+	dp.register_message_handler(show_products, commands=['show_products'])
+	
+	# оставить последним, это заглушка
+	dp.register_message_handler(plug)
 
 
 register_user_handlers()
