fb5dd5d9ff15ba55f3a7f5a538170ea9f3321b80
glebishead <glebaskayumov@yandex.ru>
2023-10-21 17:00:08 +0200

    Changed: admin.py Условное добавление товара в базу данных

--- src/handlers/__init__.py
+++ src/handlers/__init__.py
@@ -2,7 +2,10 @@
 
 """
 
-from src import dp, bot
+from aiogram.dispatcher.filters import MediaGroupFilter
+
+
+from src import dp
 from .admin import *
 from .user import *
 
@@ -13,6 +16,19 @@ def register_user_handlers():
 	# Обработчики событий от пользователя
 	dp.register_message_handler(send_contacts, commands=['contacts'])
 	dp.register_message_handler(show_products, commands=['show_products'])
+	
+	# Обработчики событий от админа
+	dp.register_message_handler(add_product, commands=['add_product'])
+	dp.register_message_handler(add_product_cancel, commands=['cancel'], state=AddProductStates.states)
+	
+	dp.register_message_handler(add_product_name, state=AddProductStates.name)
+	dp.register_message_handler(add_product_category, state=AddProductStates.category)
+	dp.register_message_handler(add_product_description, state=AddProductStates.description)
+	dp.register_message_handler(add_product_media_group, MediaGroupFilter(is_media_group=True),
+	                            content_types=['photo', 'video'], state=AddProductStates.media)
+	dp.register_message_handler(add_product_media, content_types=['photo', 'video'], state=AddProductStates.media)
+	dp.register_message_handler(add_product_price, state=AddProductStates.price)
+	dp.register_message_handler(add_product_quantity, state=AddProductStates.quantity)
 	
 	# оставить последним, это заглушка
 	dp.register_message_handler(plug)
