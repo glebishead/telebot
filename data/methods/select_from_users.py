741e146b11e91a497c0dc0d9c701c07dcc31e979
SSmartXYZ <subbotin9vlad@gmail.com>
2023-10-29 15:49:38 +0200

    Some specific changes 1) Added new table "items" 2) Fixed errors

--- data/methods/select_from_users.py
+++ data/methods/select_from_users.py
@@ -10,11 +10,11 @@ async def select_from_users():
         db = await aiomysql.connect(user='root',
                                     password='as1234dflolGG',
                                     host='78.36.203.224',
-                                    db='telegrambot')
+                                    db='db')
 
         cursor = await db.cursor()
 
-        await cursor.execute('USE telegrambot;')
+        await cursor.execute('USE db;')
 
         await cursor.execute("SELECT * FROM users;")
         return await cursor.fetchall()
