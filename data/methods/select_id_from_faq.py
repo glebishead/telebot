91d30768050cd0a195e770d6063e3d4199d43940
SSmartXYZ <subbotin9vlad@gmail.com>
2023-11-19 00:18:29 +0200

    lmao changes i guess dobavleno edit users i edit products

---
+++ data/methods/select_id_from_faq.py
@@ -0,0 +1,32 @@
+import asyncio
+import aiomysql
+
+loop = asyncio.new_event_loop()
+asyncio.set_event_loop(loop)
+
+
+async def select_from_faq(id):
+    try:
+        db = await aiomysql.connect(user='root',
+                                    password='as1234dflolGG',
+                                    host='78.36.203.224',
+                                    db='db')
+
+        cursor = await db.cursor()
+
+        await cursor.execute('USE db;')
+
+        insert_query = """SELECT questiontext, answertext FROM faq WHERE questionID = '%s';"""
+
+        await cursor.execute(insert_query, id)
+        lis = await cursor.fetchall()
+        li = lis[0]
+        return li
+
+    except aiomysql.Error as err:
+        print(err)
+
+    finally:
+        db.close()
+
+print(loop.run_until_complete(select_from_faq(2)))
