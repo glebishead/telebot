fdfba0bd6d9e8b9f73e96cc4c56aa75909c34a8e
glebishead <glebaskayumov@yandex.ru>
2023-11-24 21:43:52 +0200

    .

--- src/faq_keyboard.py
+++ src/faq_keyboard.py
@@ -1,22 +1,24 @@
 from aiogram.types import KeyboardButton, ReplyKeyboardMarkup
-from data.methods.select_id_from_faq import select_from_faq
+
+from data.methods import select_from_faq
 
 Q_COUNT = 3
 
 
-def get_faq():
+async def get_faq():
     res = {}
     for qid in range(1, Q_COUNT + 1):
-        question, answer = select_from_faq(qid)
+        question, answer = [*await select_from_faq(qid)]
         res[question] = answer
     return res
 
 
-def faq_keyboard():
+async def faq_keyboard():
     button0 = KeyboardButton("моего вопроса нет в списке")
 
     result_kb = ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True)
-    for question in get_faq().keys():
+    keys = await get_faq()
+    for question in keys.keys():
         button = KeyboardButton(question)
         result_kb.row(button)
 
